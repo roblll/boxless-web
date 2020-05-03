3b65eec97b67ae112eaf7665d98899e2c54243e5
rob <lenon.rob@gmail.com>
2020-05-03 14:18:39 -0500

    Add homescreen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Boxless</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const styles = {
+      container: {
+        display: "flex",
+        justifyContent: "center",
+      },
+      sections: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundColor: "blue",
+        height: "100vh",
+      },
+    };
+    return (
+      <div style={styles.container}>
+        <div style={styles.sections}></div>
+      </div>
+    );
+  }
 }
-
-export default App;
