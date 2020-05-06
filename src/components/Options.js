49a832a691b6a543e0da60d662ae6fc6f7436bfd
rob <lenon.rob@gmail.com>
2020-05-06 08:30:25 -0500

    Add rest of buttons to Options section

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,23 +5,86 @@ import Date from "./Date";
 import Rank from "./Rank";
 import OptionsButton from "./OptionsButton";
 
-export default class Options extends React.Component {
-  render() {
-    return (
-      <div style={styles.container}>
-        <div style={styles.optionsRow}>
-          <Date />
-        </div>
-        <div style={styles.optionsRow}>
-          <Rank />
-        </div>
-        <div style={styles.optionsRow}>
-          <OptionsButton toggle={() => {}} on={true} name="test" label="Test" />
-        </div>
+const Options = (props) => {
+  return (
+    <div style={styles.container}>
+      <div style={styles.optionsRow}>
+        <Date />
       </div>
-    );
-  }
-}
+      <div style={styles.optionsRow}>
+        <Rank />
+      </div>
+      <div style={styles.divider}></div>
+      <div style={styles.optionsRow}>
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="lyrics"
+          label="Lyrics"
+        />
+        <OptionsButton toggle={() => {}} on={true} name="clean" label="Clean" />
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="karaoke"
+          label="Karaoke"
+        />
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="norepeats"
+          label="No Repeats"
+        />
+      </div>
+      <div style={styles.divider}></div>
+      <div style={styles.genreRow}>
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="alternative"
+          label="Alternative"
+        />
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="country"
+          label="Country"
+        />
+        <OptionsButton toggle={() => {}} on={true} name="dance" label="Dance" />
+      </div>
+      <div style={styles.genreRow}>
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="electronic"
+          label="Electronic"
+        />
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="hiphop"
+          label="Hip Hop"
+        />
+        <OptionsButton toggle={() => {}} on={true} name="house" label="House" />
+      </div>
+      <div style={styles.genreRow}>
+        <OptionsButton toggle={() => {}} on={true} name="latin" label="Latin" />
+        <OptionsButton toggle={() => {}} on={true} name="pop" label="Pop" />
+        <OptionsButton toggle={() => {}} on={true} name="rap" label="Rap" />
+      </div>
+      <div style={styles.genreRow}>
+        <OptionsButton toggle={() => {}} on={true} name="randb" label="R&B" />
+        <OptionsButton toggle={() => {}} on={true} name="rock" label="Rock" />
+        <OptionsButton
+          toggle={() => {}}
+          on={true}
+          name="trance"
+          label="Trance"
+        />
+      </div>
+    </div>
+  );
+};
 
 const styles = {
   container: {
@@ -40,4 +103,16 @@ const styles = {
     justifyContent: "center",
     alignItems: "center",
   },
+  genreRow: {
+    width: "100%",
+    display: "flex",
+    flex: 1,
+  },
+  divider: {
+    width: "82%",
+    background: "#555",
+    height: "1px",
+  },
 };
+
+export default Options;
