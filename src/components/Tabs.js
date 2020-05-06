600aa81170e7497fe6be20b3b6161e9b2d1ffcf5
rob <lenon.rob@gmail.com>
2020-05-06 15:44:15 -0500

    Replace Tabs titles with icons
    Add material icons fonts in index.html

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -12,28 +12,28 @@ const Tabs = (props) => {
           active={activeTab === "options"}
           onClick={handleTabClick}
         >
-          Options
+          <i className="material-icons">music_note</i>
         </Menu.Item>
         <Menu.Item
           name="pick"
           active={activeTab === "pick"}
           onClick={handleTabClick}
         >
-          Pick
+          <i className="material-icons">touch_app</i>
         </Menu.Item>
         <Menu.Item
           name="search"
           active={activeTab === "search"}
           onClick={handleTabClick}
         >
-          Search
+          <i className="material-icons">search</i>
         </Menu.Item>
         <Menu.Item
           name="playlist"
           active={activeTab === "playlist"}
           onClick={handleTabClick}
         >
-          Playlist
+          <i className="material-icons">queue_music</i>
         </Menu.Item>
       </Menu>
     </div>
