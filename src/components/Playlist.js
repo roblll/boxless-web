a0a82f643ac64d63a896801ed24b37daf849bcd0
rob <lenon.rob@gmail.com>
2020-05-07 18:49:47 -0500

    Add contents of Playlist.js

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,7 +4,146 @@ export default class Playlist extends React.Component {
   render() {
     return (
       <div style={styles.container}>
-        <p>Playlist</p>
+        <div style={styles.scrollable}>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}>
+              music_note
+            </i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.rowSelected}>
+            <div style={styles.rowTop}>
+              <i class="material-icons" style={styles.play}></i>
+              <img
+                src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+                alt="thumbnail"
+                style={styles.image}
+              />
+              <p style={styles.title}>Title - Artist</p>
+            </div>
+            <div style={styles.rowBottom}>
+              <i class="material-icons" style={styles.button}>
+                play_arrow
+              </i>
+              <i class="material-icons" style={styles.button}>
+                keyboard_arrow_down
+              </i>
+              <i class="material-icons" style={styles.button}>
+                keyboard_arrow_up
+              </i>
+              <i class="material-icons" style={styles.button}>
+                add
+              </i>
+              <i class="material-icons" style={styles.button}>
+                remove
+              </i>
+            </div>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+          <div style={styles.row}>
+            <i class="material-icons" style={styles.play}></i>
+            <img
+              src="https://i.ytimg.com/vi/jNQXAC9IVRw/hqdefault.jpg"
+              alt="thumbnail"
+              style={styles.image}
+            />
+            <p style={styles.title}>Title - Artist</p>
+          </div>
+        </div>
       </div>
     );
   }
@@ -13,7 +152,51 @@ export default class Playlist extends React.Component {
 const styles = {
   container: {
     display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#3D3E3F",
+    color: "white",
+  },
+  scrollable: {
+    height: "330px",
+    overflow: "auto",
+  },
+  row: {
+    flex: 1,
+    display: "flex",
+    borderBottom: "1px black solid",
     alignItems: "center",
-    justifyContent: "center",
+    cursor: "pointer",
+  },
+  play: {
+    width: "45px",
+    textAlign: "center",
+  },
+  image: {
+    height: "33px",
+    margin: "10px 15px 10px 0",
+  },
+  title: {
+    margin: "0px",
+    width: "216px",
+  },
+  rowSelected: {
+    display: "flex",
+    flexDirection: "column",
+    borderBottom: "1px black solid",
+    cursor: "pointer",
+    flex: 1,
+  },
+  rowTop: {
+    display: "flex",
+    alignItems: "center",
+  },
+  rowBottom: {
+    display: "flex",
+    textAlign: "center",
+    padding: "0 33px 13px 33px",
+  },
+  button: {
+    flex: 1,
   },
 };
