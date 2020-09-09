35a2bc49f17d2a314663a52c7059ca570158a352
rob <lenon.rob@gmail.com>
2020-09-09 14:22:11 -0500

    Update Login to handle submit and post request backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,44 @@
 import React from "react";
 
 export default class Login extends React.Component {
-  handleSubmit = () => {
-    alert("aa");
+  state = {
+    initials: "",
+    phone: "",
+  };
+
+  handleChange = (key) => (event) => {
+    this.setState({ [key]: event.target.value });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { initials, phone } = this.state;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ initials, phone }),
+    };
+    const response = await fetch(
+      "http://localhost:3001/api/test",
+      requestOptions
+    );
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      const errMessage = await response.text();
+      console.log(errMessage);
+    }
   };
 
   render() {
     return (
-      // <form onSubmit={this.handleSubmit}>
       <form onSubmit={this.handleSubmit}>
         <label>
           <input
             type="text"
-            // value={this.state.firstName}
-            // onChange={this.handleChange("initials")}
+            value={this.state.initials}
+            onChange={this.handleChange("initials")}
             placeholder="Initials"
           />
         </label>
@@ -21,8 +46,8 @@ export default class Login extends React.Component {
         <label>
           <input
             type="text"
-            // value={this.state.lastName}
-            // onChange={this.handleChange("lastName")}
+            value={this.state.phone}
+            onChange={this.handleChange("phone")}
             placeholder="Phone"
           />
         </label>
