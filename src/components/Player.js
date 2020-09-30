06f6fc65eef1bbca715a98b4be40f0825ce06a39
rob <lenon.rob@gmail.com>
2020-09-30 17:52:46 -0500

    Update Player to fade start and of song

--- src/components/Player.js
+++ src/components/Player.js
@@ -18,27 +18,27 @@ class Player extends React.Component {
     return true;
   }
 
-  test = (e) => {
-    // e.target.setVolume(10);
-    // setTimeout(() => e.target.setVolume(20), 500);
-    // setTimeout(() => e.target.setVolume(30), 1000);
-    // setTimeout(() => e.target.setVolume(40), 1500);
-    // setTimeout(() => e.target.setVolume(50), 2000);
-    // setTimeout(() => e.target.setVolume(60), 2500);
-    // setTimeout(() => e.target.setVolume(70), 3000);
-    // setTimeout(() => e.target.setVolume(80), 3500);
-    // setTimeout(() => e.target.setVolume(90), 4000);
-    // setTimeout(() => e.target.setVolume(100), 4500);
-    // setTimeout(() => e.target.setVolume(90), 55500);
-    // setTimeout(() => e.target.setVolume(80), 56000);
-    // setTimeout(() => e.target.setVolume(70), 56500);
-    // setTimeout(() => e.target.setVolume(60), 57000);
-    // setTimeout(() => e.target.setVolume(50), 57500);
-    // setTimeout(() => e.target.setVolume(40), 58000);
-    // setTimeout(() => e.target.setVolume(30), 58500);
-    // setTimeout(() => e.target.setVolume(20), 59000);
-    // setTimeout(() => e.target.setVolume(10), 59500);
-    // setTimeout(() => e.target.setVolume(0), 60000);
+  fade = (e) => {
+    e.target.setVolume(10);
+    setTimeout(() => e.target.setVolume(20), 500);
+    setTimeout(() => e.target.setVolume(30), 1000);
+    setTimeout(() => e.target.setVolume(40), 1500);
+    setTimeout(() => e.target.setVolume(50), 2000);
+    setTimeout(() => e.target.setVolume(60), 2500);
+    setTimeout(() => e.target.setVolume(70), 3000);
+    setTimeout(() => e.target.setVolume(80), 3500);
+    setTimeout(() => e.target.setVolume(90), 4000);
+    setTimeout(() => e.target.setVolume(100), 4500);
+    setTimeout(() => e.target.setVolume(90), 55500);
+    setTimeout(() => e.target.setVolume(80), 56000);
+    setTimeout(() => e.target.setVolume(70), 56500);
+    setTimeout(() => e.target.setVolume(60), 57000);
+    setTimeout(() => e.target.setVolume(50), 57500);
+    setTimeout(() => e.target.setVolume(40), 58000);
+    setTimeout(() => e.target.setVolume(30), 58500);
+    setTimeout(() => e.target.setVolume(20), 59000);
+    setTimeout(() => e.target.setVolume(10), 59500);
+    setTimeout(() => e.target.setVolume(0), 60000);
   };
 
   render() {
@@ -78,7 +78,7 @@ class Player extends React.Component {
             videoId={vidId}
             opts={opts}
             onEnd={playNext}
-            onPlay={this.test}
+            onPlay={this.fade}
           />
         ) : (
           <Static height={height} width={width} />
