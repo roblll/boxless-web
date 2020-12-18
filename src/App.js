e3c07abfa54cddd7192ef71b07885a4e2f9b793d
rob <lenon.rob@gmail.com>
2020-12-18 15:52:29 -0600

    Update App to add state and continuePlaying property

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  state = {
+    continuePlaying: true,
+  };
+
   render() {
     return <div></div>;
   }
