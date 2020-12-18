0a765cac59304c35d4439702c74ba25c69ae26a0
rob <lenon.rob@gmail.com>
2020-12-18 16:07:54 -0600

    Update App to bring back Player

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
+import Player from "./components/Player";
+
 export default class App extends Component {
   state = {
     continuePlaying: true,
   };
 
   render() {
-    return <div></div>;
+    return (
+      <div style={styles.container}>
+        <Player vidId={"QYh6mYIJG2Y"} />
+      </div>
+    );
   }
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "black",
+  },
+  bottomPadding: {
+    height: "50px",
+    backgroundColor: "#3D3E3F",
+  },
+};
