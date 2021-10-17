af78dd5081cbe19340bf72546b2b93263cddc61f
rob <lenon.rob@gmail.com>
2021-10-16 23:17:57 -0500

    Update App.js to add and use handlePlayPause

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { Button } from "semantic-ui-react";
 import Player from "./components/Player";
 
 export default class App extends Component {
+  handlePlayPause = () => {
+    alert("test");
+  };
+
   render() {
     return (
       <div style={styles.container}>
         <Player vidId={"d-JBBNg8YKs"} />
-        <Button onClick={() => alert("test")}>Play</Button>
+        <Button onClick={this.handlePlayPause}>Play</Button>
       </div>
     );
   }
