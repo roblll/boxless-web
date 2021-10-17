aa17f2e666630e6ecfff8b72b68a3be968253837
rob <lenon.rob@gmail.com>
2021-10-16 22:45:40 -0500

    Update App.js to add container style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return <div style={styles.container}></div>;
   }
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "black",
+  },
+};
